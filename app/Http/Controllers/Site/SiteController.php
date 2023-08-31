f648ce4c327de9098913dd27e0b30530d0b88ba7
RenanPiccinini <renanpiccinini1990@gmail.com>
2023-08-31 15:24:46 -0300

    layout-ok

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Site;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Categoria;
+use App\Models\Filme;
 use App\Services\Site\FilmesSiteService;
 
 class SiteController extends Controller
@@ -16,6 +17,7 @@ class SiteController extends Controller
 
     public function home()
     {
+        $dez_filmes = Filme::orderBy('created_at', 'desc')->paginate(10);
         $filmes = $this->filmesSiteService->filmes();
         $todos_filmes = $this->filmesSiteService->todosFilmes();
         $categorias = Categoria::orderBy('nome_categoria')->get();
@@ -23,7 +25,8 @@ class SiteController extends Controller
         return view('site.home', [
             'filmes' => $filmes,
             'categorias' => $categorias,
-            'todos_filmes' => $todos_filmes
+            'todos_filmes' => $todos_filmes,
+            'dez_filmes' => $dez_filmes
         ]);
     }
 }
